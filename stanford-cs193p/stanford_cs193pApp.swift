09efbebc4da5fafd675f17dbcc9db59c69141119
Nick Hendryx <nphendryx@gmail.com>
2024-09-19 19:59:18 -0700

    Initial Commit

---
+++ stanford-cs193p/stanford_cs193pApp.swift
@@ -0,0 +1,17 @@
+//
+//  stanford_cs193pApp.swift
+//  stanford-cs193p
+//
+//  Created by Nicholas Hendryx on 9/19/24.
+//
+
+import SwiftUI
+
+@main
+struct stanford_cs193pApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ stanford-cs193pTests/stanford_cs193pTests.swift
@@ -0,0 +1,17 @@
+//
+//  stanford_cs193pTests.swift
+//  stanford-cs193pTests
+//
+//  Created by Nicholas Hendryx on 9/19/24.
+//
+
+import Testing
+@testable import stanford_cs193p
+
+struct stanford_cs193pTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
